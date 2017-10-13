2aa87a18ed2a8b6129d414af8bedace1f9773e23
xaquille <andreanaradyka@gmail.com>
2017-10-13 10:48:32 +0700

    second

--- xaqmap.py
+++ xaqmap.py
@@ -8,6 +8,15 @@ from datetime import datetime
 #clear the screen
 subprocess.call('clear',shell=True)
 
+#How to use
+print "-" * 60
+print "# HOW TO USE" + " " * 47 + "#"
+print "# $python xaqmap.py" + " " * 40 + "#"
+print "# host :127.0.0.1" + " " * 42 + "#"
+print "# range scan port from : 0" + " " * 33 + "#"
+print "# range scan port to : 1000" + " " * 32 + "#"
+print "-" * 60
+
 #input
 remoteServer = raw_input("host :")
 remoteServerIP = socket.gethostbyname(remoteServer)
@@ -15,23 +24,26 @@ remotePort = int (raw_input ("range scan port from : "))
 remotePort2 = int (raw_input ("range scan port to : "))
 
 #print banner
+print ""
+print ""
 print "=" * 60
-print "please wait", remoteServerIP
+print "scan IP ", remoteServerIP
 print "=" * 60
+print ""
 
 #start checking time
 firstTime=datetime.now()
+
 
 #scan
 try:
     for port in range(remotePort,remotePort2):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         result = sock.connect_ex((remoteServerIP,port))
-        #banner = sock.recv(1024)
         if result == 0:
-            print "Port {}:Open".format(port)
+            print "Port {} Open".format(port) + " " ,(sock.recv(1024))
             sock.close()
-except KeyboardInterruptet :
+except KeyboardInterrupt:
         print "You stoped it"
         sys.exit()
 
